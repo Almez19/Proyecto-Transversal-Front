97e3e6affb4e6a01129f5d96aa5c626935ca2801
COMPU-S031\daw2 <alfmezquia@gmail.com>
2026-02-10 17:37:13 +0100

    commit

--- TransversalAngular/src/app/service/noticias-service.ts
+++ TransversalAngular/src/app/service/noticias-service.ts
@@ -33,7 +33,7 @@ export class NoticiasService {
   public getUltimasNoticias (): Promise<NoticiasInterface[]>{
 
     return this.noticiasPromise = lastValueFrom(
-      this.httpClient.get<NoticiasInterface[]>(this.baseURL+'ultimas')
+      this.httpClient.get<NoticiasInterface[]>(this.baseURL+"ultimas")
     );
 
   }
